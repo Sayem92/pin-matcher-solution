2cedaaa13ffc3a8b64d32b18e2af7f9e92f52cd4
Sayem92 <sayemmiha123@gmail.com>
2022-08-19 11:01:56 +0600

    add clear and backspace

--- js/app.js
+++ js/app.js
@@ -22,3 +22,26 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
 })
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number == 'C'){
+            typedNumberField.value = '';
+        }
+        else if(number == '<'){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainigDigits = digits.join('');
+            typedNumberField.value = remainigDigits;
+
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+    
+})
