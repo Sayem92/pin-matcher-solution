9efc1e04295e0d072f57324085a25c212182f990
Sayem92 <sayemmiha123@gmail.com>
2022-08-19 10:23:26 +0600

    add pin generate

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function getPin(){
+    const pin = generataPin();
+    const pinString = pin +'';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        // console.log('pin not 3 digit found',pin)
+       return getPin();
+    }
+}
+
+
+function generataPin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    //display pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+})
